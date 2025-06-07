c4f747720f41dbae70ab4d7e724f2a721ea7de4a
Nicolas <Nicomar94@hotmail.com>
2025-06-07 11:38:13 -0300

    Trabajo practico 12 realizado

---
+++ 12 Datos Avanzados/Trabajo Practico 12 - Nicolas Gonzalez.py
@@ -0,0 +1,26 @@
+#1
+
+precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva':
+1450}
+
+print(precios_frutas)
+
+precios_frutas['Naranja'] = 1200
+precios_frutas['Manzana'] = 1500
+precios_frutas['Pera'] = 2300
+
+print(precios_frutas)
+
+#2
+
+precios_frutas['Banana'] = 1330
+precios_frutas['Manzana'] = 1700
+precios_frutas['Melon'] = 2800
+
+print(precios_frutas)
+
+#3
+
+Solo_frutas= [precios_frutas.keys()]
+
+print(Solo_frutas)
